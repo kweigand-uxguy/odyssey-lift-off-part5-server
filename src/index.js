98fea639be786dfab71a09eec7c4a8fbab8337fd
kweigand-uxguy <kenweigand.uxguy@gmail.com>
2023-01-24 13:29:49 -0600

    turn on introspection

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    introspection: process.env.NODE_ENV !== 'production',
     dataSources: () => {
       return {
         trackAPI: new TrackAPI(),
